71443d5b4ad93eb4d4934a08dc2d1adbab1047eb
Felipe Vieira <nvieirafelipe@gmail.com>
2014-04-15 13:13:30 -0300

    Load rvm and android tools on fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,17 @@
 set -g -x PATH ~/.bin $PATH
 
+# Android
+set -g -x PATH ~/Developer/android/sdk/platform-tools $PATH
+set -g -x PATH ~/Developer/android/sdk/tools $PATH
+
 set fish_path ~/.oh-my-fish/
 set fish_plugins git php localhost emoji-clock
 . $fish_path/oh-my-fish.fish
+
+# RVM
+rvm > /dev/null
+
+. ~/.config/fish/functions/aliases.fish
 
 function fish_greeting
   echo
@@ -10,5 +19,3 @@ function fish_greeting
   echo
   envup
 end
-
-. ~/.config/fish/functions/aliases.fish
