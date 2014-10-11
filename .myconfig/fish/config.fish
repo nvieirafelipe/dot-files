9390b84ab6cb08a48d09a3d16b18615f8b8e509a
Felipe Vieira <nvieirafelipe@gmail.com>
2014-10-11 14:43:46 -0300

    Update fish and Sublime config

--- .myconfig/fish/config.fish
+++ .myconfig/fish/config.fish
@@ -1,17 +1,17 @@
 set -g -x PATH ~/.bin $PATH
 
 # Android
-set -g -x PATH ~/Developer/android/sdk/platform-tools $PATH
-set -g -x PATH ~/Developer/android/sdk/tools $PATH
+set -g -x PATH ~/Developer/android-sdk/sdk/platform-tools $PATH
+set -g -x PATH ~/Developer/android-sdk/sdk/tools $PATH
 
 set fish_path ~/.oh-my-fish/
 set fish_plugins git php localhost emoji-clock
 . $fish_path/oh-my-fish.fish
 
+. ~/.config/fish/functions/aliases.fish
+
 # RVM
 rvm > /dev/null
-
-. ~/.config/fish/functions/aliases.fish
 
 function fish_greeting
   echo
--- .myconfig/fish/functions/envup.fish
+++ .myconfig/fish/functions/envup.fish
@@ -1,5 +1,5 @@
 function envup
   if test -f '.env'
-    sed -Ee 's/export (.*)=(.*)$/set -gx \1 \2/' .env | .
+    sed -Ee 's/export (.*)=(.*)$/set -gx \1 \2/' .env | . > /dev/null 2>&1
   end
 end
