2b83b96b9eccd00c9f616aac623387a8a3dd871e
Felipe Vieira <nvieirafelipe@gmail.com>
2014-12-05 11:42:05 -0200

    Update alias

--- .myconfig/fish/functions/aliases.fish
+++ .myconfig/fish/functions/aliases.fish
@@ -1,22 +1,18 @@
 # git
-
 alias ga="git add"
-
 alias gb="git branch"
-
 alias gc="git commit"
-
-alias gd="git diff"
-
-alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-
 alias gco="git checkout"
-
+alias gd="git diff"
+alias gf="git fetch"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+alias gp="git push"
+alias gr="git remote"
+alias grc="git rebase --continue"
 alias gs="git status"
 
-alias gp="git push"
-
-alias gf="git fetch"
+# workspace
+alias developer="cd ~/Developer"
 
 # test suite
 
